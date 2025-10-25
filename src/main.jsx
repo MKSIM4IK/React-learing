906d1d715b19be84e57518680fc7e1a9a9be748d
MAKSIM4IK <yuriipalii2@gmail.com>
2025-10-25 13:50:20 +0200

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 
-const google = (
-  <a href="https://www.google.com">
-    Go to Google
-  </a>
-);
+const person = {
+  name: 'John Doe',
+  tel: '123-456-7890',
+  city: 'New York'
+};
 
-const apple = (
-  <a href="https://www.apple.com">
-    Go to Apple Website
-  </a>
+const NewPerson = {
+  name: 'Jane Smith',
+  tel: '987-654-3210',
+  city: 'Los Angeles'
+};
+
+const Tel = (props) => <a href={`tel:${props.tel}`}>{props.tel}</a>;
+
+const element = (
+  <>
+    <h1>{person.name}</h1>
+    <Tel tel={NewPerson.tel} />
+    <p>{person.city}</p>
+  </>
 );
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {google},
-    {apple}
+    {element}
   </StrictMode>
 );
